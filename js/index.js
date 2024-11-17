845146a2ef6d962eab9cced8faaef3c048a3f220
fgarciaaltare <fgarciaaltare@udesa.edu.ar>
2024-11-17 13:15:57 -0300

    cree los js y avanze con el form

--- js/index.js
+++ js/index.js
@@ -1,9 +1,47 @@
-fetch('https://dummyjson.com/recipes?limit=10')
+fetch('https://dummyjson.com/recipes')
 .then (function(response){
     return response.json();
 })
-.then(function(data) {
+.then(
+    function(data){
     console.log(data);
-    let imagen = document.querySelector(".imagen")
-    imagen.src = data.recipes[0].image
+    let recetas = document.querySelector(".recetas")
+    let titulo = ""
+
+    for (let i=0; i<10; i++) {
+        console.log(i)
+        titulo += `<article class="imagenes">
+        <img src=${data.recipes[i].image} alt = "">
+        <a class="comida" href="./index.html?id=${dara.recipes[i].id}">
+        Comida:${data.recipes[i].name}</a>
+        <p class="dificultad" >Dificultad: ${data.reipes[i].difficulty}</p>
+        </article>`;
+    }
+    recetas.innerHTML=titulo
+
+    let cargar = document.querySelector('.button_cargar')
+    cargar.addEventListener('click', function(){
+        for(let i=10;i<20;i++){
+            titulo += `<article class="imagenes">
+            <img src=${data.recipes[i].image} alt = ""><div>
+            <a class="comida" href="./index.html?id=${data.recipes[i].id}"
+            Comida: ${data.recipes[i].name}</a>
+            <p class="dificultad" >Dificultad: ${data.recipes[i].difficulty}</p></div>
+            </article>`;
+            recetas.innerHTML=titulo
+        }
+    })
+}
+)
+.catch(function(error)
+{
+    console.log(error)
 })
+
+fetch('https://dummyjson.com/recipes/search?q=Margherita')
+.then(res => res.json())
+.then(function(buscar){
+    console.log(buscar);
+    let buscador = document.querySelector(`.button_cargar`)
+    buscador = `<a>./detalles.html</a>`
+});
