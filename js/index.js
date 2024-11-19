8d5f575193b19fe272a3c08accf2e854dcafa3f6
male <malenarodriguez@udesa.edu.ar>
2024-11-19 15:43:23 -0300

    termine de cargar las imagenes con fetch en el index

--- js/index.js
+++ js/index.js
@@ -1,47 +1,50 @@
 fetch('https://dummyjson.com/recipes')
-.then (function(response){
+.then(function(response) {
     return response.json();
 })
-.then(
-    function(data){
+.then(function(data) {
     console.log(data);
-    let recetas = document.querySelector(".recetas")
-    let titulo = ""
+    let recetas = document.querySelector(".recetas");
+    let titulo = "";
 
-    for (let i=0; i<10; i++) {
-        console.log(i)
+    for (let i = 0; i < 10; i++) {
+        console.log(i);
         titulo += `<article class="imagenes">
-        <img src=${data.recipes[i].image} alt = "">
-        <a class="comida" href="./index.html?id=${dara.recipes[i].id}">
-        Comida:${data.recipes[i].name}</a>
-        <p class="dificultad" >Dificultad: ${data.reipes[i].difficulty}</p>
+        <img src="${data.recipes[i].image}" alt="">
+        <a class="comida" href="./index.html?id=${data.recipes[i].id}">
+        Comida: ${data.recipes[i].name}</a>
+        <p class="dificultad">Dificultad: ${data.recipes[i].difficulty}</p>
         </article>`;
     }
-    recetas.innerHTML=titulo
+    recetas.innerHTML = titulo;
 
-    let cargar = document.querySelector('.button_cargar')
-    cargar.addEventListener('click', function(){
-        for(let i=10;i<20;i++){
+    let cargar = document.querySelector('.button_cargar');
+    cargar.addEventListener('click', function() {
+        for (let i = 10; i < 20; i++) {
             titulo += `<article class="imagenes">
-            <img src=${data.recipes[i].image} alt = ""><div>
-            <a class="comida" href="./index.html?id=${data.recipes[i].id}"
+            <img src="${data.recipes[i].image}" alt="">
+            <a class="comida" href="./index.html?id=${data.recipes[i].id}">
             Comida: ${data.recipes[i].name}</a>
-            <p class="dificultad" >Dificultad: ${data.recipes[i].difficulty}</p></div>
+            <p class="dificultad">Dificultad: ${data.recipes[i].difficulty}</p>
             </article>`;
-            recetas.innerHTML=titulo
         }
-    })
-}
-)
-.catch(function(error)
-{
-    console.log(error)
+        recetas.innerHTML = titulo;
+    });
 })
+.catch(function(error) {
+    console.log("Error al obtener las recetas:", error);
+});
 
 fetch('https://dummyjson.com/recipes/search?q=Margherita')
-.then(res => res.json())
-.then(function(buscar){
+.then(function(res) {
+    return res.json();
+})
+.then(function(buscar) {
     console.log(buscar);
-    let buscador = document.querySelector(`.button_cargar`)
-    buscador = `<a>./detalles.html</a>`
+    let buscador = document.querySelector('.buscador');
+    buscador.innerHTML = `<a href="./detalles.html?id=${buscar.recipes[0].id}">Ver detalles de Margherita</a>`;
+})
+.catch(function(error) {
+    console.log("Error al buscar Margherita:", error);
 });
+
